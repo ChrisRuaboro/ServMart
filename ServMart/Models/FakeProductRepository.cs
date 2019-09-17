1f61391f38123ec1f5d37b6164ed607fbae0006d
wwstudent <wwstudent@MS-9D12CH2>
2019-09-17 11:58:17 -0700

    Add project files.

---
+++ ServMart/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServMart.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product> {
+        new Product { Name = "Disinfectant Wipes", Price = 5 },
+        new Product { Name = "Mop Bucket", Price = 115 },
+        new Product { Name = "Six Part Folder", Price = 5 },
+        new Product { Name = "Fabuloso", Price = 10 },
+        }.AsQueryable<Product>();
+    }
+}
---
+++ ServMart/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ServMart.Models;
+using ServMart.Models.ViewModels;
+
+namespace ServMart.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
+
